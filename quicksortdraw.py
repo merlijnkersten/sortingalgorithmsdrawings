96b48654d3deafdf7afb3a5f1808b1bdbb8af868
merlijnkersten <kerstenmerlijn@gmail.com>
2019-09-13 20:43:58 +0100

    Update quicksortdraw.py

--- quicksortdraw.py
+++ quicksortdraw.py
@@ -1,45 +1,43 @@
 # See read me
-'''
+
 import random
 
-anne = list(range(0,5))
+def find_first_sublist(sublist):
+    length = len(sublist)
+    for index in range(0, len(anne)):
+        if anne[index:index+length] == sublist:
+            return index, index+length
+def replace_sublist(sublist, replacement):
+    length = len(replacement)
+    index = 0
+    for start, end in iter(lambda: find_    first_sublist(sublist), None):
+        anne[start:end] = replacement
+        index = start + length
 
+def xavier(charlie): #Bob: before, Charlie: to be sorted, Emily: after.
+    #print(charlie)
+    dave = []
+    dave.extend(charlie)
+    fransesca = [] # smaller or equal than pivot
+    pivot = dave[-1]
+    for i in range(len(dave)-1):
+        if dave[i] <= pivot:
+            fransesca.append(dave[i])
+            charlie.remove(dave[i])
+        print(fransesca + charlie)
+    replace_sublist(dave, [fransesca, pivot, charlie])
+    #fransesca.append(pivot)
+    #charlie.remove(pivot)
+    #print(fransesca + charlie)
+
+
+anne = list(range(0,10))
 print(anne)
-
 random.shuffle(anne)
-
+xavier(anne)
 print(anne)
+#           0 1 2 3 4 5 6 7
 
-def ziggy(bob):
-    cas = []
-    deidre = []
-    last_item = bob[-1]
-    for i in range(len(bob)-1):
-        if bob[i] < last_item:
-            cas.append(bob[i])
-        else:
-            deidre.append(bob[i])
-    cas.append(last_item)
-    bob = cas + deidre
-    print(bob)
-
-anne = ziggy(anne)
-'''
-
-def xavier(bob, charlie, emily): #Bob: before, Charlie: to be sorted, Emily: after.
-    print(bob + charlie + emily)
-    ogcharlie = charlie
-    fransesca = [] # smaller or equal than Charlie
-    pivot = ogcharlie[-1]
-    for i in range(len(ogcharlie)-1):
-        if ogcharlie[i] <= pivot:
-           fransesca.append(ogcharlie[i])
-           charlie.remove(ogcharlie[i])
-        print(bob + fransesca + charlie + emily)
-    fransesca.append(pivot)
-    print(bob + fransesca + charlie + emily)
-
-xavier([], [1,4,2,7,3,5,7,3], [])
 
 
 
